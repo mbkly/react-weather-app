d9da9640ffeda6bdc0ebbf3db903969c7c5c4ee2
Mary Buckley <99931390+mbkly@users.noreply.github.com>
2022-12-19 16:13:37 +0100

    Initial HTML structure for weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,44 @@
 import React from "react";
+import "./Weather.css";
 
-export function Weather() {
-  return <h2>Hello World</h2>;
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Zürich</h1>
+      <ul>
+        <li>Monday 16:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Partly Cloudy"
+          ></img>
+          2°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 82%</li>
+            <li>Wind: 2 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
