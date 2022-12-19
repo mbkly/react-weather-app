eebe10003944e95207d56f03354b1169f86c3ef7
Mary Buckley <99931390+mbkly@users.noreply.github.com>
2022-12-19 13:54:24 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://mary-buckley-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mary Buckley
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/mbkly/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://mary-buckley-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mary Buckley
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/mbkly/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
